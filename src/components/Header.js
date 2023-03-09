834026bceb8fdce2468f96399c459eeed40e3152
Aleksa Marinkovic <aleksa.marinkovic456@gmail.com>
2023-03-09 21:55:26 +0100

    Started working on responsive header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,86 +62,94 @@ const Header = () => {
   }, []);
   return (
     <Navbar
+      collapseOnSelect
+      expand="xl"
       sticky="top"
       className={`${
         small ? "header-container-wrapper-small" : "header-container-wrapper"
       }`}
-      collapseOnSelect
-      expand="lg"
     >
-      <Nav className="me-auto">
-        <HashLink
-          smooth
-          to="/#top"
-          className={`${
-            small ? "header-image-container-small" : "header-image-container"
-          }`}
-        >
-          <img
-            src={require("../images/dem-logo-desktop.webp")}
-            className="header-logo-image"
-            alt="HOME"
-          ></img>
-        </HashLink>
-      </Nav>
+      <HashLink
+        smooth
+        to="/#top"
+        className={`${
+          small ? "header-image-container-small" : "header-image-container"
+        }`}
+      >
+        <img
+          src={require("../images/dem-logo-desktop.webp")}
+          className="header-logo-image"
+          alt="HOME"
+        ></img>
+      </HashLink>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-      <div className="header-horizontal-container">
-        <NavHashLink smooth to={"/#top"} className="header-item-container">
-          NASLOVNA
-        </NavHashLink>
-        {isBusy && !fetchError && (
-          <div className="header-item-container" disabled>PROIZVODI</div>
-        )}
-        {fetchError && (
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <NavHashLink smooth to={"/#top"} className="header-item-container">
+            NASLOVNA
+          </NavHashLink>
+          {isBusy && !fetchError && (
+            <div className="header-item-container" disabled>
+              PROIZVODI
+            </div>
+          )}
+          {fetchError && (
+            <NavHashLink
+              smooth
+              to={"/products#top"}
+              className="header-item-container"
+            >
+              PROIZVODI
+            </NavHashLink>
+          )}
+          {!isBusy && (
+            <NavDropdown
+              title="PROIZVODI"
+              id="collasible-nav-dropdown"
+              className="header-item-container"
+            >
+              <NavDropdown.Item
+                className="header-item-container-collapsable"
+                href="/#/products#top"
+              >
+                SVI PROIZVODI
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+                className="header-item-container-collapsable"
+                disabled
+              >
+                PO KATEGORIJAMA:
+              </NavDropdown.Item>
+              {categories.map((category) => {
+                const url = "/#/products/categories/" + category.categoryName;
+                return (
+                  <NavDropdown.Item
+                    href={url}
+                    className="header-item-container-collapsable"
+                  >
+                    {category.categoryName}
+                  </NavDropdown.Item>
+                );
+              })}
+            </NavDropdown>
+          )}
           <NavHashLink
             smooth
-            to={"/products#top"}
+            to={"/info#top"}
             className="header-item-container"
           >
-            PROIZVODI
+            KONTAKT
           </NavHashLink>
-        )}
-        {!isBusy && (
-          <NavDropdown
-            title="PROIZVODI"
-            id="collasible-nav-dropdown"
+          <NavHashLink
+            smooth
+            to={"/service#top"}
             className="header-item-container"
           >
-            <NavDropdown.Item className="header-item-container-collapsable" href="/#/products#top">
-              SVI PROIZVODI
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-                  className="header-item-container-collapsable"
-                  disabled
-                >
-                  PO KATEGORIJAMA:
-                </NavDropdown.Item>
-            {categories.map((category) => {
-              const url = "/#/products/categories/" + category.categoryName;
-              return (
-                <NavDropdown.Item
-                  href={url}
-                  className="header-item-container-collapsable"
-                >
-                  {category.categoryName}
-                </NavDropdown.Item>
-              );
-            })}
-          </NavDropdown>
-        )}
-        <NavHashLink smooth to={"/info#top"} className="header-item-container">
-          KONTAKT
-        </NavHashLink>
-        <NavHashLink
-          smooth
-          to={"/service#top"}
-          className="header-item-container"
-        >
-          SERVIS
-        </NavHashLink>
-      </div>
+            SERVIS
+          </NavHashLink>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
